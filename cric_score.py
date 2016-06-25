37d73a65caed842a341d232e67676df690b87cef
Akshdeep singh rajawat <rajawatakshdeep@gmail.com>
2016-06-25 19:09:24 +0530

    updated program
    Updated with notification function to show notififcations on desktop

--- cric_score.py
+++ cric_score.py
@@ -1,6 +1,7 @@
 #! usr/bin/python3
 import requests
 from bs4 import BeautifulSoup
+import os
 
 res = requests.get("http://www.cricbuzz.com/cricket-match/live-scores")
 
@@ -11,6 +12,7 @@ print("\n\n\n\n")
 #print(soup.find_all("a",{"class":"cb-lv-scrs-well-live"})[0].text)
 for item in soup.find_all("a",{"class":"cb-lv-scrs-well-live"}):
 	print("\t\t\t"+item.text)	
+	os.system('notify-send  "Live Cricket Score" "%s"' % (item.text) )
 
 print("\n\n\n")
 print("\n\n\n powered by cricbuzz.com")
